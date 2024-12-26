30da1710c5cfcf1681223d2e745fdf8ee15cb56e
vedantb3 <bansalvedant3@gmail.com>
2024-12-26 14:34:39 +0530

    add test for empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,4 +2,13 @@ require 'rspec'
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  describe '#add' do
+    context 'when input string is empty ' do
+      it 'returns 0' do
+        expect(calculator.add('')).to eq(0)
+      end
+    end
+  end
 end
