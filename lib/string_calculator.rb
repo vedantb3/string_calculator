00aebe47e6a9969c138124e46c1e050e58b74d4d
vedantb3 <bansalvedant3@gmail.com>
2024-12-26 17:45:45 +0530

    handle string has negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,6 +2,10 @@ class StringCalculator
   def add(string)
     return 0 if string.empty?
 
-    string.split(/,|\n|;/).map(&:to_i).sum
+    numbers = string.split(/,|\n|;/).map(&:to_i)
+    negative_numbers = numbers.select(&:negative?)
+    raise "negative numbers not allowed #{negative_numbers.join(',')}" unless negative_numbers.empty?
+
+    numbers.sum
   end
 end
